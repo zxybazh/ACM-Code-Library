1cec6f8c082f1fc810e4d33cf2d520d97bf051c3
zxybazh <zxybazh@qq.com>
2013-03-10 20:57:09 +0800

    add module of cpp

---
+++ _.cpp
@@ -0,0 +1,37 @@
+#include <map>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <cstring>
+#include <cstdlib>
+#include <utility>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define LL long long
+#define LD long double
+#define fo(i, l, r) for (int i = l; i <= r; ++i)
+#define fd(i, r, l) for (int i = r; i >= l; --i)
+#define Fo(i, r) for (int i = 1; i <= r; ++i)
+#define Fd(i, l) for (int i = n; i >= l; --i)
+#define rep(i, l, r) for (int i = l; i < r; ++i)
+#define rem(i, r, l) for (int i = r - 1; i >= l; --i)
+#define VI vector<int>
+#define PB push_back
+
+int n, m;
+
+int main(){
+    //freopen("in.txt", "r", stdin);
+    //freopen("ou.txt", "w", stdout);
+    cin >> n;
+
+    return 0;
+}
